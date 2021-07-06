12bb36eb4c48665f635a98c2b24adfc72061242c
Sergey Malinovskiy <malinovskiy.sergey.olegovich@gmail.com>
2021-07-06 11:02:55 +0300

    Поправил шаблон и разделил на секции

--- app/config/web.php
+++ app/config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Cистема тех. учета',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
