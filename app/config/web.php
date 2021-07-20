0c3fcfee0b6e65af7fe47948b2c6f4912c3076a2
SergeyMalinovskiy <malinovskiy.sergey.olegovich@gmail.com>
2021-07-20 10:30:14 +0300

    Изменил язык приложения

--- app/config/web.php
+++ app/config/web.php
@@ -8,6 +8,7 @@ $config = [
     'name' => 'Cистема тех. учета',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language'  => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
